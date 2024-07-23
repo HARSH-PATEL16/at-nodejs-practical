e9accc6dcc5b97bfd2b0188d0f2f439ac2b3bba9
Harsh <harshpatel16000@gmail.com>
2024-07-23 12:28:51 +0530

    removed unwanted code from the server file

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,21 @@
-// NODE FRAMEWORK
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const bodyParser = require('body-parser') // TO PARSE POST REQUEST
-const cors = require('cors') // ALLOW CROSS ORIGIN REQUESTS
+const cors = require('cors');
 require('dotenv').config();
 require('./Database/db_connection');
-app.use(cors())
-app.use(express.json())
 
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.text({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // Routers
 const userRoutes =  require('./Routes/user');
-app.use(userRoutes)
+app.use(userRoutes);
 
 const PORT = process.env.PORT || 3000
-//server
+// Server
 server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
-})
+});
